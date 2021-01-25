4c429ed39141150ab7151775f9adb4173c02a345
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-01-25 21:36:29 +0900

    Avoid reversing the String

--- Roman to Integer/Roman to Integer.playground/Contents.swift
+++ Roman to Integer/Roman to Integer.playground/Contents.swift
@@ -1,5 +1,5 @@
-// Runtime: 24 ms, faster than 76.52% of Swift online submissions for Roman to Integer.
-// Memory Usage: 14.6 MB, less than 42.33% of Swift online submissions for Roman to Integer.
+// Runtime: 20 ms, faster than 89.62% of Swift online submissions for Roman to Integer.
+// Memory Usage: 14.8 MB, less than 21.73% of Swift online submissions for Roman to Integer.
 
 import UIKit
 
@@ -16,16 +16,19 @@ class Solution {
         
         var total = 0, prevVal = 0
         
-        for char in s.reversed() {
-            if dic[char]! >= prevVal {
-                prevVal = dic[char]!
-                total += prevVal
-            } else {
-                prevVal = dic[char]!
+        for char in s {
+            let val = dic[char]!
+            if prevVal < val {
                 total -= prevVal
+            } else {
+                total += prevVal
             }
+            
+            prevVal = val
         }
         
+        total += prevVal // Last character is always summed up
+
         return total
     }
 }
