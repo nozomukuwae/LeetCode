31145d42f2bfcfd4f947b40bafd40369b16317e6
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-01-25 21:03:03 +0900

    Evaluate backwards
    Use dictionary to map Roman and Integer.

--- Roman to Integer/Roman to Integer.playground/Contents.swift
+++ Roman to Integer/Roman to Integer.playground/Contents.swift
@@ -1,3 +1,6 @@
+// Runtime: 36 ms, faster than 31.15% of Swift online submissions for Roman to Integer.
+// Memory Usage: 14.6 MB, less than 42.33% of Swift online submissions for Roman to Integer.
+
 import UIKit
 
 let s = "MCMXCIII"
@@ -8,89 +11,25 @@ print(num)
 
 
 class Solution {
+    let dic: [Character: Int] = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
+    
     func romanToInt(_ s: String) -> Int {
-        var chars = Array(s)
-        var num = 0
+        var chars = Array(s.reversed())
+        var lastChar = chars[0]
         
-        loop:
-        while chars.count > 0 {
-            switch chars[0] {
-            case "I":
-                if (chars.count > 1) {
-                    switch chars[1] {
-                    case "X":
-                        num += 9
-                    case "V":
-                        num += 4
-                    default: // Only "I" comes here and after
-                        num += 2
-                    }
-                    
-                    chars.removeFirst(2)
-                } else {
-                    return num + 1
-                }
-            case "V":
-                num += 5
-                chars.removeFirst()
-            case "X":
-                if (chars.count > 1) {
-                    switch chars[1] {
-                    case "C":
-                        num += 90
-                    case "L":
-                        num += 40
-                    case "X":
-                        num += 20
-                    case "V":
-                        num += 15
-                    default: // I
-                        num += 10
-                        chars.removeFirst()
-                        continue loop // Re-evaluate "I" again in next cycle, in case that "X""V" comes after "I"
-                    }
-                    
-                    chars.removeFirst(2)
-                } else {
-                    return num + 10
-                }
-            case "L":
-                num += 50
-                chars.removeFirst()
-            case "C":
-                if (chars.count > 1) {
-                    switch chars[1] {
-                    case "M":
-                        num += 900
-                    case "D":
-                        num += 400
-                    case "C":
-                        num += 200
-                    case "L":
-                        num += 150
-                    case "V":
-                        num += 105
-                    default: // X, I
-                        num += 100
-                        chars.removeFirst()
-                        continue loop // Re-evaluate again in next cycle
-                    }
-                    
-                    chars.removeFirst(2)
-                } else {
-                    return num + 100
-                }
-            case "D":
-                num += 500
-                chars.removeFirst()
-            case "M":
-                num += 1000
-                chars.removeFirst()
-            default:
-                fatalError()
+        var total = dic[lastChar]! // Last character is always summed up
+        chars.removeFirst()
+        
+        for char in chars {
+            if dic[char]! >= dic[lastChar]! {
+                total += dic[char]!
+            } else {
+                total -= dic[char]!
             }
+
+            lastChar = char
         }
         
-        return num
+        return total
     }
 }
