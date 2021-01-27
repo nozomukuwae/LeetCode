0673652105002f87b1a0a33b4d842a4ae4257f9c
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-01-27 20:15:34 +0900

    [Two Sum] Implement one-pass solution

--- Two Sum/Two Sum.playground/Contents.swift
+++ Two Sum/Two Sum.playground/Contents.swift
@@ -1,23 +1,23 @@
 // Runtime: 36 ms, faster than 73.06% of Swift online submissions for Two Sum.
-// Memory Usage: 14 MB, less than 76.34% of Swift online submissions for Two Sum.
+// Memory Usage: 14.5 MB, less than 13.51% of Swift online submissions for Two Sum.
 
 
 import UIKit
 
 let solution = Solution()
-let num = solution.twoSum([3,2,4], 6)
+let num = solution.twoSum([3,3], 6)
 print(num)
 
 class Solution {
     func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        var dict: [Int: Int] = [:]
+        
         for i in (0 ..< numbers.count) {
-            let wantedValue = target - numbers[i]
-            
-            for j in (i + 1 ..< numbers.count) {
-                if numbers[j] == wantedValue {
-                    return [i, j]
-                }
+            if let j = dict[target - numbers[i]] {
+                return [j, i]
             }
+            
+            dict[numbers[i]] = i
         }
         
         fatalError()
