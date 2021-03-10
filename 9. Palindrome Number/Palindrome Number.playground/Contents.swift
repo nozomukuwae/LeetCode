93d3149e92c5797e97d3b5655561adc273dc46bd
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-03-10 18:22:18 +0900

    [Palindrome Number] Revert the number in stead of creating digit array

--- 9. Palindrome Number/Palindrome Number.playground/Contents.swift
+++ 9. Palindrome Number/Palindrome Number.playground/Contents.swift
@@ -1,31 +1,29 @@
-//Runtime: 40 ms, faster than 82.67% of Swift online submissions for Palindrome Number.
-//Memory Usage: 14 MB, less than 74.67% of Swift online submissions for Palindrome Number.
+//Runtime: 52 ms, faster than 54.50% of Swift online submissions for Palindrome Number.
+//Memory Usage: 14.5 MB, less than 21.33% of Swift online submissions for Palindrome Number.
 
 import UIKit
 
 let solution = Solution()
-let x = 123
+let x = 0
 print(solution.isPalindrome(x))
 
 class Solution {
     func isPalindrome(_ x: Int) -> Bool {
-        if x < 0 {
+        if x < 0 || (x % 10 == 0 && x != 0) {
             return false
         }
         
         var n = x
-        var digits = [Int]()
-        while n > 0 {
-            digits.append(n % 10)
+        var reverted = 0
+        while reverted < n {
+            reverted = reverted * 10 + n % 10
             n /= 10
         }
         
-        for i in 0 ..< digits.count / 2 {
-            if digits[i] != digits[digits.count - 1 - i] {
-                return false
-            }
+        if reverted / 10 == n || reverted == n {
+            return true
+        } else {
+            return false
         }
-                
-        return true
     }
 }
