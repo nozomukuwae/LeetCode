df64eca5501a3c6a94208d69d15009bbc4a06099
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-03-20 21:20:44 +0900

    [Maximum Subarray] Greedy solution

--- 53. Maximum Subarray/Maximum Subarray.playground/Contents.swift
+++ 53. Maximum Subarray/Maximum Subarray.playground/Contents.swift
@@ -1,5 +1,5 @@
-//Runtime: 60 ms, faster than 15.65% of Swift online submissions for Maximum Subarray.
-//Memory Usage: 14.4 MB, less than 22.52% of Swift online submissions for Maximum Subarray.
+//Runtime: 32 ms, faster than 87.02% of Swift online submissions for Maximum Subarray.
+//Memory Usage: 14.3 MB, less than 48.28% of Swift online submissions for Maximum Subarray.
 
 import UIKit
 
@@ -8,34 +8,14 @@ print(solution.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
 
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        return maxSubArray(nums, start: 0, end: nums.count - 1)
-    }
-    
-    func maxSubArray(_ nums: [Int], start: Int, end: Int) -> Int {
-        if start >= end {
-            return nums[start]
-        }
-               
-        return max(maxSubArray(nums, start: start, end: (start + end) / 2),
-                   maxSubArray(nums, start: (start + end) / 2 + 1, end: end),
-                   crossMax(nums, start: start, end: end))
-    }
-    
-    func crossMax(_ nums: [Int], start: Int, end: Int) -> Int {
-        var leftCurrentSum = 0
-        var leftMax = Int.min
-        for i in stride(from: (start + end) / 2, through: start, by: -1) {
-            leftCurrentSum += nums[i]
-            leftMax = max(leftMax, leftCurrentSum)
+        var currentSum = nums[0]
+        var maxSum = nums[0]
+        
+        for i in 1 ..< nums.count {
+            currentSum = max(currentSum + nums[i], nums[i])
+            maxSum = max(maxSum, currentSum)
         }
         
-        var rightCurrentSum = 0
-        var rightMax = Int.min
-        for i in (start + end) / 2 + 1 ... end {
-            rightCurrentSum += nums[i]
-            rightMax = max(rightMax, rightCurrentSum)
-        }
-        
-        return leftMax + rightMax
+        return maxSum
     }
 }
