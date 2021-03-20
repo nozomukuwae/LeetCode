b804fd1afefe92dce7da10db64414ffbe62c614b
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-03-20 20:49:11 +0900

    [Maximum Subarray] Divide & conquer solution

--- 53. Maximum Subarray/Maximum Subarray.playground/Contents.swift
+++ 53. Maximum Subarray/Maximum Subarray.playground/Contents.swift
@@ -1,5 +1,5 @@
-//Runtime: 1820 ms, faster than 5.15% of Swift online submissions for Maximum Subarray.
-//Memory Usage: 14.5 MB, less than 10.88% of Swift online submissions for Maximum Subarray.
+//Runtime: 64 ms, faster than 13.55% of Swift online submissions for Maximum Subarray.
+//Memory Usage: 14.3 MB, less than 48.28% of Swift online submissions for Maximum Subarray.
 
 import UIKit
 
@@ -8,18 +8,38 @@ print(solution.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
 
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        var maxSum = nums[0]
-        
-        for i in 0 ..< nums.count {
-            var sum = nums[i]
-            maxSum = max(maxSum, sum)
-            
-            for j in i + 1 ..< nums.count {
-                sum += nums[j]
-                maxSum = max(maxSum, sum)
-            }
+        return maxSubArray(nums, start: 0, end: nums.count - 1)
+    }
+    
+    func maxSubArray(_ nums: [Int], start: Int, end: Int) -> Int {
+        if start >= end {
+            return nums[start]
         }
         
-        return maxSum
+        return max(maxSubArray(nums, start: start, end: (start + end) / 2),
+                   maxSubArray(nums, start: (start + end) / 2 + 1, end: end),
+                   crossMax(nums, start: start, end: end))
+    }
+    
+    func crossMax(_ nums: [Int], start: Int, end: Int) -> Int {
+        if start >= end {
+            return nums[start]
+        }
+        
+        var leftCurrentSum = 0
+        var leftMax = Int.min
+        for i in stride(from: (start + end) / 2, through: start, by: -1) {
+            leftCurrentSum += nums[i]
+            leftMax = max(leftMax, leftCurrentSum)
+        }
+        
+        var rightCurrentSum = 0
+        var rightMax = Int.min
+        for i in (start + end) / 2 + 1 ... end {
+            rightCurrentSum += nums[i]
+            rightMax = max(rightMax, rightCurrentSum)
+        }
+        
+        return leftMax + rightMax
     }
 }
