6d82f0f27ef862ee8a56566ec276793c94707481
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-03 21:22:03 +0900

    [Container With Most Water] Store max heights into new arrays

--- 11. Container With Most Water/Container With Most Water.playground/Contents.swift
+++ 11. Container With Most Water/Container With Most Water.playground/Contents.swift
@@ -1,5 +1,4 @@
-//Runtime: 2512 ms, faster than 5.11% of Swift online submissions for Container With Most Water.
-//Memory Usage: 18.1 MB, less than 24.68% of Swift online submissions for Container With Most Water.
+//Time Limit Exceeded
 
 // Time complexity: O(N^2)
 // Space complexity: O(1)
@@ -14,25 +13,38 @@ print(solution.maxArea([1,8,6,2,5,4,8,3,7]))
 
 class Solution {
     func maxArea(_ height: [Int]) -> Int {
-        var maxI = 0
-        var maxJ = 0
+        var maxFromStart = [[Int]]()
+        var maxFromEnd = [[Int]]()
         var maxArea = 0
         
+        var maxHeightFromStart = 0
         for i in 0 ..< height.count - 1 {
-            if height[i] <= maxI {
+            if height[i] <= maxHeightFromStart {
                 continue
             }
-            maxI = height[i]
+            
+            maxHeightFromStart = height[i]
+            maxFromStart.append([i, height[i]])
+        }
+        
+        var maxHeightFromEnd = 0
+        var j = height.count - 1
+        while j > 0 {
+            if height[j] > maxHeightFromEnd {
+                maxHeightFromEnd = height[j]
+                maxFromEnd.append([j, height[j]])
+            }
 
-            maxJ = 0
-            var j = height.count - 1
-            while j > i {
-                if height[j] > maxJ {
-                    maxJ = height[j]
-                    maxArea = max(maxArea, min(height[i], height[j]) * (j - i))
+            j -= 1
+        }
+        
+        for k in 0 ..< maxFromStart.count {
+            for l in 0 ..< maxFromEnd.count {
+                if maxFromStart[k][0] >= maxFromEnd[l][0] {
+                    continue
                 }
 
-                j -= 1
+                maxArea = max(maxArea, min(maxFromStart[k][1], maxFromEnd[l][1]) * (maxFromEnd[l][0] - maxFromStart[k][0]))
             }
         }
         
