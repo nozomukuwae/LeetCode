eb7ca0d875a03cc95fa5c901d808ecca9de1f10d
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-03 20:14:56 +0900

    [Valid Mountain Array] Remove a variable to decrease memory usage

--- 941. Valid Mountain Array/Valid Mountain Array.playground/Contents.swift
+++ 941. Valid Mountain Array/Valid Mountain Array.playground/Contents.swift
@@ -1,5 +1,5 @@
 //Runtime: 208 ms, faster than 87.28% of Swift online submissions for Valid Mountain Array.
-//Memory Usage: 14.3 MB, less than 76.30% of Swift online submissions for Valid Mountain Array.
+//Memory Usage: 14.1 MB, less than 87.86% of Swift online submissions for Valid Mountain Array.
 
 // Time complexity: O(N)
 // Space complexity: O(1)
@@ -13,29 +13,19 @@ print(solution.validMountainArray([0,3,2,1]))
 
 class Solution {
     func validMountainArray(_ arr: [Int]) -> Bool {
-        var isIncreasing = true
-        
-        for i in 1 ..< arr.count {
-            switch isIncreasing {
-            case true:
-                if arr[i - 1] < arr[i] {
-                    continue
-                } else if arr[i - 1] > arr[i] {
-                    if i <= 1 {
-                        return false
-                    }
-                    
-                    isIncreasing = false
-                } else {
-                    return false
-                }
-            case false:
-                if arr[i - 1] <= arr[i] {
-                    return false
-                }
-            }
+        var i = 1
+        while i < arr.count && arr[i - 1] < arr[i] {
+            i += 1
         }
         
-        return isIncreasing ? false : true
+        if i == 1 || i == arr.count {
+            return false
+        }
+        
+        while i < arr.count && arr[i - 1] > arr[i] {
+            i += 1
+        }
+        
+        return (i == arr.count)
     }
 }
