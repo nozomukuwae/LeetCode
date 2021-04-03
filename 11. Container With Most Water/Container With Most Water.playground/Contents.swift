f0bba7ea7c8f7e3860daf617afb7d35782bc638f
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-03 20:53:47 +0900

    [Container With Most Water] Decrease steps by introducing max height from start and end of the array

--- 11. Container With Most Water/Container With Most Water.playground/Contents.swift
+++ 11. Container With Most Water/Container With Most Water.playground/Contents.swift
@@ -1,4 +1,5 @@
-//Time Limit Exceeded
+//Runtime: 2956 ms, faster than 5.11% of Swift online submissions for Container With Most Water.
+//Memory Usage: 18.1 MB, less than 24.68% of Swift online submissions for Container With Most Water.
 
 // Time complexity: O(N^2)
 // Space complexity: O(1)
@@ -9,14 +10,29 @@ let solution = Solution()
 print(solution.maxArea([1,1]))
 print(solution.maxArea([4,3,2,1,4]))
 print(solution.maxArea([1,2,1]))
+print(solution.maxArea([1,8,6,2,5,4,8,3,7]))
 
 class Solution {
     func maxArea(_ height: [Int]) -> Int {
+        var maxI = 0
+        var maxJ = 0
         var maxArea = 0
         
         for i in 0 ..< height.count {
-            for j in i ..< height.count {
-                maxArea = max(maxArea, min(height[i], height[j]) * (j - i))
+            if height[i] <= maxI {
+                continue
+            }
+            maxI = max(maxI, height[i])
+
+            maxJ = 0
+            var j = height.count - 1
+            while j > i {
+                if height[j] > maxJ {
+                    maxJ = max(maxJ, height[j])
+                    maxArea = max(maxArea, min(height[i], height[j]) * (j - i))
+                }
+
+                j -= 1
             }
         }
         
