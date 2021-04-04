4549c2bb4607771120e187cf3424a39e8c30aba0
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-04 18:45:15 +0900

    [Longest Substring Without Repeating Characters] Avoid converting to char array

--- 3. Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.playground/Contents.swift
+++ 3. Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.playground/Contents.swift
@@ -1,5 +1,4 @@
-//Runtime: 240 ms, faster than 22.07% of Swift online submissions for Longest Substring Without Repeating Characters.
-//Memory Usage: 14.9 MB, less than 46.85% of Swift online submissions for Longest Substring Without Repeating Characters.
+//Time Limit Exceeded
 
 // Time complexity: O(N^2)
 // Space complexity: O(N)
@@ -17,15 +16,16 @@ print(solution.lengthOfLongestSubstring("dvdf")) // 3
 
 class Solution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        let charArray = Array(s)
         var maxSubstringLength = 0
         
         var start = 0
         var end = 0
-        while end < charArray.count {
-            if let index = charArray[start ..< end].lastIndex(of: charArray[end]) {
+        while end < s.count {
+            if let index = s[String.Index(utf16Offset: start, in: s) ..< String.Index(utf16Offset: end, in: s)]
+                .lastIndex(of: s[String.Index(utf16Offset: end, in: s)]) {
+                
                 maxSubstringLength = max(maxSubstringLength, end - start)
-                start = index + 1
+                start = s.distance(from: s.startIndex, to: index) + 1
             }
             
             end += 1
