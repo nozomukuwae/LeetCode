f627f6bb8d3a8cbf88952e3b29af8f74f08089db
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-07 18:15:43 +0900

    [Find First and Last Position of Element in Sorted Array] Binary search (partially) - add 1 variable and decrease steps

--- 34. Find First and Last Position of Element in Sorted Array/Find First and Last Position of Element in Sorted Array.playground/Contents.swift
+++ 34. Find First and Last Position of Element in Sorted Array/Find First and Last Position of Element in Sorted Array.playground/Contents.swift
@@ -1,5 +1,5 @@
 //Runtime: 48 ms, faster than 65.51% of Swift online submissions for Find First and Last Position of Element in Sorted Array.
-//Memory Usage: 15.1 MB, less than 50.63% of Swift online submissions for Find First and Last Position of Element in Sorted Array.
+//Memory Usage: 15.1 MB, less than 68.67% of Swift online submissions for Find First and Last Position of Element in Sorted Array.
 
 // Time complexity: O(N)
 // Space complexity: O(1)
@@ -11,24 +11,28 @@ print(solution.searchRange([5,7,7,8,8,10], 8))
 print(solution.searchRange([5,7,7,8,8,10], 6))
 print(solution.searchRange([], 0))
 print(solution.searchRange([1], 1))
+print(solution.searchRange([1,2,2,3,4,4,4], 4))
 
 class Solution {
     func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
         var start = 0
         var end = nums.count - 1
+        var mid: Int!
         
         while start <= end {
-            if nums[(start + end) / 2] < target {
-                start = (start + end) / 2 + 1
-            } else if nums[(start + end) / 2] > target {
-                end = (start + end) / 2 - 1
+            mid = (start + end) / 2
+            
+            if nums[mid] < target {
+                start = mid + 1
+            } else if nums[mid] > target {
+                end = mid - 1
             } else { // nums[mid] == target
-                var i = (start + end) / 2 - 1
+                var i = mid - 1
                 while i >= 0 && nums[i] == target {
                     i -= 1
                 }
                 
-                var j = (start + end) / 2 + 1
+                var j = mid + 1
                 while j < nums.count && nums[j] == target {
                     j += 1
                 }
