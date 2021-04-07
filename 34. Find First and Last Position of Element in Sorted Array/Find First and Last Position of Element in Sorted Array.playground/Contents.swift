c51efc31d190b37ee05942bba820703c520d344e
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-07 18:34:44 +0900

    [Find First and Last Position of Element in Sorted Array] Binary search (comprehensively)

--- 34. Find First and Last Position of Element in Sorted Array/Find First and Last Position of Element in Sorted Array.playground/Contents.swift
+++ 34. Find First and Last Position of Element in Sorted Array/Find First and Last Position of Element in Sorted Array.playground/Contents.swift
@@ -1,7 +1,7 @@
-//Runtime: 48 ms, faster than 65.51% of Swift online submissions for Find First and Last Position of Element in Sorted Array.
-//Memory Usage: 15.1 MB, less than 68.67% of Swift online submissions for Find First and Last Position of Element in Sorted Array.
+//Runtime: 44 ms, faster than 96.52% of Swift online submissions for Find First and Last Position of Element in Sorted Array.
+//Memory Usage: 15.4 MB, less than 15.19% of Swift online submissions for Find First and Last Position of Element in Sorted Array.
 
-// Time complexity: O(N)
+// Time complexity: O(logN)
 // Space complexity: O(1)
 
 import Foundation
@@ -18,7 +18,9 @@ class Solution {
         var start = 0
         var end = nums.count - 1
         var mid: Int!
+        var firstTargetIndex = -1
         
+        // Loop until finding the first target
         while start <= end {
             mid = (start + end) / 2
             
@@ -27,20 +29,39 @@ class Solution {
             } else if nums[mid] > target {
                 end = mid - 1
             } else { // nums[mid] == target
-                var i = mid - 1
-                while i >= 0 && nums[i] == target {
-                    i -= 1
+                if mid == 0 || nums[mid - 1] != target {
+                    firstTargetIndex = mid
+                    break
+                } else {
+                    end = mid - 1
                 }
-                
-                var j = mid + 1
-                while j < nums.count && nums[j] == target {
-                    j += 1
-                }
-                
-                return [i + 1, j - 1]
             }
         }
         
-        return [-1, -1]
+        if firstTargetIndex < 0 {
+            return [-1, -1]
+        }
+        
+        start = firstTargetIndex
+        end = nums.count - 1
+
+        // Loop until finding the last target
+        while start <= end {
+            mid = (start + end) / 2
+            
+            if nums[mid] < target {
+                start = mid + 1
+            } else if nums[mid] > target {
+                end = mid - 1
+            } else { // nums[mid] == target
+                if mid == nums.count - 1 || nums[mid + 1] != target {
+                    return [firstTargetIndex, mid]
+                } else {
+                    start = mid + 1
+                }
+            }
+        }
+        
+        return [firstTargetIndex, firstTargetIndex]
     }
 }
