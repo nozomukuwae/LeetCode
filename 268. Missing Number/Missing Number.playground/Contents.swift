91cda42bb617eec8049551a60ce223c53c91cb22
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-10 17:36:09 +0900

    Use Gauss's Formula

--- 268. Missing Number/Missing Number.playground/Contents.swift
+++ 268. Missing Number/Missing Number.playground/Contents.swift
@@ -1,8 +1,8 @@
-//Runtime: 200 ms, faster than 14.92% of Swift online submissions for Missing Number.
+//Runtime: 136 ms, faster than 77.87% of Swift online submissions for Missing Number.
 //Memory Usage: 14.5 MB, less than 31.90% of Swift online submissions for Missing Number.
 
 // Time complexity: O(N)
-// Space complexity: O(N)
+// Space complexity: O(1)
 
 import Foundation
 
@@ -14,9 +14,6 @@ print(solution.missingNumber([0])) // 1
 
 class Solution {
     func missingNumber(_ nums: [Int]) -> Int {
-        var dictionary = [Int: Int]()
-        nums.indices.map { dictionary[nums[$0]] = $0 }
-        
-        return (0 ... nums.count).first(where: { dictionary[$0] == nil })!
+        return nums.count * (nums.count + 1) / 2 - nums.reduce(0, +)
     }
 }
