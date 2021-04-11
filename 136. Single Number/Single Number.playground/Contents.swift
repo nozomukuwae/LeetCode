ae61f4267dce4d19bb782907b576eb65166d2654
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-11 17:41:49 +0900

    [Single Number] Use Set

--- 136. Single Number/Single Number.playground/Contents.swift
+++ 136. Single Number/Single Number.playground/Contents.swift
@@ -1,7 +1,7 @@
-//Runtime: 228 ms, faster than 9.02% of Swift online submissions for Single Number.
-//Memory Usage: 14.6 MB, less than 92.71% of Swift online submissions for Single Number.
+//Runtime: 136 ms, faster than 71.79% of Swift online submissions for Single Number.
+//Memory Usage: 14.7 MB, less than 81.38% of Swift online submissions for Single Number.
 
-// Time complexity: O(NlogN)
+// Time complexity: O(N)
 // Space complexity: O(N)
 
 import Foundation
@@ -14,36 +14,7 @@ print(solution.singleNumber([-336,513,-560,-481,-174,101,-997,40,-527,-784,-283,
 
 class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
-        if nums.count <= 1 {
-            return nums[0]
-        }
-        
-        let sortedArray = nums.sorted()
-        
-        var start = 0, end = sortedArray.count - 1
-        var mid: Int!
-        
-        while start < end {
-            mid = (start + end) / 2
-            if mid % 2 == 0 {
-                if sortedArray[mid] == sortedArray[mid + 1] {
-                    start = mid + 1
-                } else {
-                    end = mid - 1
-                }
-            } else {
-                if sortedArray[mid - 1] == sortedArray[mid] {
-                    start = mid + 1
-                } else {
-                    end = mid - 1
-                }
-            }
-        }
-        
-        if start % 2 == 0 {
-            return sortedArray[start]
-        } else {
-            return sortedArray[start + 1]
-        }
+        let unique = Set(nums)
+        return 2 * unique.reduce(0, +) - nums.reduce(0, +)
     }
 }
