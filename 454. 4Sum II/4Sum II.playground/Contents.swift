cfd82e28ec53948d7823d75ce346cb78240ebea5
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-18 20:31:21 +0900

    [4Sum II] Create 2 hashmaps and then count

--- 454. 4Sum II/4Sum II.playground/Contents.swift
+++ 454. 4Sum II/4Sum II.playground/Contents.swift
@@ -1,7 +1,8 @@
-//Time Limit Exceeded
+//Runtime: 752 ms, faster than 5.71% of Swift online submissions for 4Sum II.
+//Memory Usage: 14.2 MB, less than 97.14% of Swift online submissions for 4Sum II.
 
 // Time complexity: O(N^4)
-// Space complexity: O(N^4)
+// Space complexity: O(N^2)
 
 import Foundation
 
@@ -11,20 +12,34 @@ print(solution.fourSumCount([0],[0],[0],[0]))
 
 class Solution {
     func fourSumCount(_ nums1: [Int], _ nums2: [Int], _ nums3: [Int], _ nums4: [Int]) -> Int {
-        var count = 0
+        var firstSumDict = [Int: Int]()
         
-        for i in 0 ..< nums1.count {
-            for j in 0 ..< nums2.count {
-                for k in 0 ..< nums3.count {
-                    for l in 0 ..< nums4.count {
-                        if nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0 {
-                            count += 1
-                        }
-                    }
-                }
+        var i = 0
+        var j: Int! = nil
+        while i < nums1.count {
+            j = 0
+            while j < nums2.count {
+                firstSumDict[nums1[i] + nums2[j], default: 0] += 1
+                j += 1
             }
+            
+            i += 1
         }
         
-        return count
+        var secondSumDict = [Int: Int]()
+        i = 0
+        while i < nums3.count {
+            j = 0
+            while j < nums4.count {
+                secondSumDict[nums3[i] + nums4[j], default: 0] += 1
+                j += 1
+            }
+            
+            i += 1
+        }
+
+        return firstSumDict.reduce(0) { (result, arg1) -> Int in
+            return result + arg1.value * secondSumDict[-arg1.key, default: 0]
+        }
     }
 }
