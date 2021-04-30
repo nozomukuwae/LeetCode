8234f354a4b916f69bf9b3e4330f8e8be2373a05
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-04-30 19:50:29 +0900

    [Directed Graph] Implement adjacency matrix

--- Learning/Graphs/Directed Graph.playground/Contents.swift
+++ Learning/Graphs/Directed Graph.playground/Contents.swift
@@ -4,6 +4,7 @@ let al = AdjacencyList()
 al.insertEdge(1, 2)
 al.insertEdge(2, 3)
 al.insertEdge(4, 5)
+print("Adjacency List")
 al.printGraph()
 
 class AdjacencyList {
@@ -21,6 +22,39 @@ class AdjacencyList {
         for key in map.keys {
             for value in map[key]! {
                 print("\(key) -> \(value)")
+            }
+        }
+    }
+}
+
+let am = AdjacencyMatrix(size: 5)
+am.insertEdge(0, 1)
+am.insertEdge(1, 2)
+am.insertEdge(3, 4)
+print("Adjacency Matrix")
+am.printGraph()
+
+class AdjacencyMatrix {
+    var matrix: [[Int]]
+    var size: Int
+    
+    init (size: Int) {
+        self.size = size
+        matrix = [[Int]](repeating: [Int](repeating: 0, count: size), count: size)
+    }
+    
+    func insertEdge(_ v1: Int, _ v2: Int) {
+        if (0 ..< size).contains(v1) && (0 ..< size).contains(v2) {
+            matrix[v1][v2] = 1
+        }
+    }
+
+    func printGraph() {
+        for i in 0 ..< size {
+            for j in 0 ..< size {
+                if matrix[i][j] == 1 {
+                    print("\(i) -> \(j)")
+                }
             }
         }
     }
