3f85e125460b5ec551eead59b8240a21a3fa610f
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-05-02 19:34:59 +0900

    [Merge Two Sorted Lists] Avoid node initialization to save memory

--- 21. Merge Two Sorted Lists/Merge Two Sorted Lists.playground/Contents.swift
+++ 21. Merge Two Sorted Lists/Merge Two Sorted Lists.playground/Contents.swift
@@ -1,75 +1,78 @@
-//Runtime: 16 ms, faster than 20.79% of Swift online submissions for Merge Two Sorted Lists.
-//Memory Usage: 14.1 MB, less than 36.75% of Swift online submissions for Merge Two Sorted Lists.
+//Runtime: 12 ms, faster than 82.76% of Swift online submissions for Merge Two Sorted Lists.
+//Memory Usage: 13.7 MB, less than 80.43% of Swift online submissions for Merge Two Sorted Lists.
 
-import UIKit
+// Time complexity: O(N+M)
+// Space complexity: O(1)
+
+import Foundation
 
 let solution = Solution()
-let l1 = ListNode(1, ListNode(2, ListNode(4)))
-let l2 = ListNode(1, ListNode(3, ListNode(4)))
+let node1 = ListNodeUtil.create([1,2,4])
+let node2 = ListNodeUtil.create([1,3,4])
 
-print(solution.mergeTwoLists(l1, l2)!.toString())
+//let node1 = ListNodeUtil.create([])
+//let node2 = ListNodeUtil.create([])
+
+let merged = solution.mergeTwoLists(node1, node2)
+ListNodeUtil.printNodes(merged)
+
+class ListNodeUtil {
+    static func create(_ array: [Int]) -> ListNode? {
+        var node: ListNode? = nil
+        for e in array.reversed() {
+            node = ListNode(e, node)
+        }
+        
+        return node
+    }
+    
+    static func printNodes(_ head: ListNode?) {
+        print(head?.val)
+        if let next = head?.next {
+            printNodes(next)
+        }
+    }
+}
 
 class Solution {
     func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
         if l1 == nil { return l2 }
         if l2 == nil { return l1 }
         
-        var p1 = l1
-        var p2 = l2
-        let mergedList = ListNode()
-        
-        if p1!.val < p2!.val {
-            mergedList.val = p1!.val
-            p1 = p1!.next
+        var firstNode: ListNode? = nil
+        var node1 = l1
+        var node2 = l2
+        if node1!.val < node2!.val {
+            firstNode = node1
+            node1 = node1!.next
         } else {
-            mergedList.val = p2!.val
-            p2 = p2!.next
+            firstNode = node2
+            node2 = node2!.next
         }
-
-        var currentNode: ListNode? = mergedList
-
-        while p1 != nil && p2 != nil {
-            if p1!.val < p2!.val {
-                currentNode!.next = ListNode(p1!.val)
-                p1 = p1!.next
+        var prevNode = firstNode
+        
+        while node1 != nil && node2 != nil {
+            if node1!.val < node2!.val {
+                prevNode!.next = node1
+                prevNode = node1
+                node1 = node1!.next
             } else {
-                currentNode!.next = ListNode(p2!.val)
-                p2 = p2!.next
+                prevNode!.next = node2
+                prevNode = node2
+                node2 = node2!.next
             }
-            
-            currentNode = currentNode!.next
         }
         
-        currentNode!.next = p1 ?? p2
+        prevNode!.next = node1 ?? node2
         
-        return mergedList
+        return firstNode
     }
 }
 
 public class ListNode {
     public var val: Int
     public var next: ListNode?
-    
-    public init() {
-        self.val = 0
-        self.next = nil
-    }
-    
-    public init(_ val: Int) {
-        self.val = val
-        self.next = nil
-    }
-    
-    public init(_ val: Int, _ next: ListNode?) {
-        self.val = val
-        self.next = next
-    }
-    
-    func toString() -> String {
-        if let next = self.next {
-            return String(self.val) + "," + next.toString()
-        } else {
-            return String(self.val)
-        }
-    }
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
 }
