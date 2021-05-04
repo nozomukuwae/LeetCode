18db476a5783eeca2163834b77ce36ec9bd228cd
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-05-04 17:53:50 +0900

    [79. Word Search] Slightly decrease steps

--- 79. Word Search/Word Search.playground/Contents.swift
+++ 79. Word Search/Word Search.playground/Contents.swift
@@ -1,5 +1,5 @@
-//Runtime: 3256 ms, faster than 18.01% of Swift online submissions for Word Search.
-//Memory Usage: 14.2 MB, less than 82.92% of Swift online submissions for Word Search.
+//Runtime: 3208 ms, faster than 18.94% of Swift online submissions for Word Search.
+//Memory Usage: 14.8 MB, less than 43.79% of Swift online submissions for Word Search.
 
 // Time complexity: O(NMW) where N = length of board, M = length of string in board, W = length of word
 // Space complexity: O(max(N, C)) where C = number of corresponding characters in board
@@ -20,6 +20,8 @@ class Solution {
         for i in 0 ..< board.count {
             for j in 0 ..< board[i].count {
                 if board[i][j] == charArray[0] {
+                    if charArray.count <= 1 { return true }
+                    
                     history[[j, i]] = true
                     if exist(board, charArray, 1, j - 1, i, &history) { return true }
                     if exist(board, charArray, 1, j + 1, i, &history) { return true }
@@ -34,15 +36,15 @@ class Solution {
     }
     
     func exist(_ board: [[Character]], _ charArray: [Character], _ charIndex: Int, _ x: Int, _ y: Int, _ history: inout [[Int]: Bool]) -> Bool {
-        if charIndex >= charArray.count { return true }
-
         if x < 0 { return false }
         if x >= board[0].count { return false }
         if y < 0 { return false }
         if y >= board.count { return false }
         if history[[x, y]] != nil { return false }
-        
+
         if board[y][x] == charArray[charIndex] {
+            if charIndex >= charArray.count - 1 { return true }
+
             history[[x, y]] = true
             if exist(board, charArray, charIndex + 1, x - 1, y, &history) { return true }
             if exist(board, charArray, charIndex + 1, x + 1, y, &history) { return true }
