61ccb9109b1a4eb306b916420e897e24f5795a96
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-05-04 18:21:29 +0900

    [79. Word Search] Directly access Character from String

--- 79. Word Search/Word Search.playground/Contents.swift
+++ 79. Word Search/Word Search.playground/Contents.swift
@@ -1,5 +1,5 @@
-//Runtime: 3208 ms, faster than 18.94% of Swift online submissions for Word Search.
-//Memory Usage: 14.8 MB, less than 43.79% of Swift online submissions for Word Search.
+//Runtime: 4152 ms, faster than 10.24% of Swift online submissions for Word Search.
+//Memory Usage: 14.6 MB, less than 63.35% of Swift online submissions for Word Search.
 
 // Time complexity: O(NMW) where N = length of board, M = length of string in board, W = length of word
 // Space complexity: O(max(N, C)) where C = number of corresponding characters in board
@@ -14,19 +14,19 @@ print(solution.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "A
 
 class Solution {
     func exist(_ board: [[Character]], _ word: String) -> Bool {
-        let charArray = Array(word)
-        var history = [[Int]: Bool]()
-
+        let firstChar = word[word.startIndex]
+        var history = [[Int]: Void]()
+        
         for i in 0 ..< board.count {
             for j in 0 ..< board[i].count {
-                if board[i][j] == charArray[0] {
-                    if charArray.count <= 1 { return true }
-                    
-                    history[[j, i]] = true
-                    if exist(board, charArray, 1, j - 1, i, &history) { return true }
-                    if exist(board, charArray, 1, j + 1, i, &history) { return true }
-                    if exist(board, charArray, 1, j, i - 1, &history) { return true }
-                    if exist(board, charArray, 1, j, i + 1, &history) { return true }
+                if board[i][j] == firstChar {
+                    if word.count <= 1 { return true }
+
+                    history[[j, i]] = ()
+                    if exist(board, word, 1, j - 1, i, &history) { return true }
+                    if exist(board, word, 1, j + 1, i, &history) { return true }
+                    if exist(board, word, 1, j, i - 1, &history) { return true }
+                    if exist(board, word, 1, j, i + 1, &history) { return true }
                     history[[j, i]] = nil
                 }
             }
@@ -35,24 +35,22 @@ class Solution {
         return false
     }
     
-    func exist(_ board: [[Character]], _ charArray: [Character], _ charIndex: Int, _ x: Int, _ y: Int, _ history: inout [[Int]: Bool]) -> Bool {
-        if x < 0 { return false }
-        if x >= board[0].count { return false }
-        if y < 0 { return false }
-        if y >= board.count { return false }
-        if history[[x, y]] != nil { return false }
+    func exist(_ board: [[Character]], _ word: String, _ charIndex: Int, _ x: Int, _ y: Int, _ history: inout [[Int]: Void]) -> Bool {
+        if x < 0 || x >= board[0].count || y < 0 || y >= board.count || history[[x, y]] != nil {
+            return false
+        }
 
-        if board[y][x] == charArray[charIndex] {
-            if charIndex >= charArray.count - 1 { return true }
+        if board[y][x] == word[word.index(word.startIndex, offsetBy: charIndex)] {
+            if charIndex >= word.count - 1 { return true }
 
-            history[[x, y]] = true
-            if exist(board, charArray, charIndex + 1, x - 1, y, &history) { return true }
-            if exist(board, charArray, charIndex + 1, x + 1, y, &history) { return true }
-            if exist(board, charArray, charIndex + 1, x, y - 1, &history) { return true }
-            if exist(board, charArray, charIndex + 1, x, y + 1, &history) { return true }
+            history[[x, y]] = ()
+            if exist(board, word, charIndex + 1, x - 1, y, &history) { return true }
+            if exist(board, word, charIndex + 1, x + 1, y, &history) { return true }
+            if exist(board, word, charIndex + 1, x, y - 1, &history) { return true }
+            if exist(board, word, charIndex + 1, x, y + 1, &history) { return true }
             history[[x, y]] = nil
         }
-            
+
         return false
     }
 }
