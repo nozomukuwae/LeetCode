cf3884a294e9a8b1b246422f87cb4e6a8074a34e
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-05-05 16:56:51 +0900

    [39. Combination Sum] Stop considering minimum candidates

--- 39. Combination Sum/Combination Sum.playground/Contents.swift
+++ 39. Combination Sum/Combination Sum.playground/Contents.swift
@@ -1,5 +1,5 @@
-//Runtime: 48 ms, faster than 42.81% of Swift online submissions for Combination Sum.
-//Memory Usage: 13.8 MB, less than 92.81% of Swift online submissions for Combination Sum.
+//Runtime: 32 ms, faster than 64.39% of Swift online submissions for Combination Sum.
+//Memory Usage: 14 MB, less than 75.18% of Swift online submissions for Combination Sum.
 
 // Time complexity: O(M^N) where N = count of candidates, M = max quotient of target / candidates
 // Space complexity: O(M^N)
@@ -16,32 +16,29 @@ print(solution.combinationSum([1], 2))
 
 class Solution {
     func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
-        let minVal = candidates.min()! // time: O(N)
         var combinations = [[Int]]()
         
-        // Find divisors in candidates
-        for i in 0 ..< candidates.count { // time: O(N)
-            if target % candidates[i] == 0 {
-                combinations.append([Int](repeating: candidates[i], count: target / candidates[i]))
-            }
-        }
-        
-        let maxCount = candidates[0] == minVal ? (target - minVal - 1) / candidates[0] : (target - minVal) / candidates[0]
+        let maxCount = target / candidates[0]
         var i = 0
         while i <= maxCount {
-            findCombination(candidates, target - minVal, minVal, 1, &combinations, target - candidates[0] * i, [Int](repeating: candidates[0], count: i))
+            if candidates[0] * i == target {
+                combinations.append([Int](repeating: candidates[0], count: i))
+            } else {
+                findCombination(candidates, 1, &combinations, target - candidates[0] * i, [Int](repeating: candidates[0], count: i))
+            }
+            
             i += 1
         }
 
         return combinations
     }
     
-    func findCombination(_ candidates: [Int], _ upperLimit: Int, _ minVal: Int, _ index: Int, _ combinations: inout [[Int]], _ currentTarget: Int, _ currentCombination: [Int]) {
+    func findCombination(_ candidates: [Int], _ index: Int, _ combinations: inout [[Int]], _ currentTarget: Int, _ currentCombination: [Int]) {
         
         guard currentTarget > 0 else { return }
         guard index < candidates.count else { return }
         
-        let maxCount = candidates[index] == minVal ? (upperLimit - 1) / candidates[index] : upperLimit / candidates[index]
+        let maxCount = currentTarget / candidates[index]
         var current = currentCombination
         var i = 0
                 
@@ -49,7 +46,7 @@ class Solution {
             if candidates[index] * i == currentTarget {
                 combinations.append(current)
             } else {
-                findCombination(candidates, upperLimit, minVal, index + 1, &combinations, currentTarget - candidates[index] * i, current)
+                findCombination(candidates, index + 1, &combinations, currentTarget - candidates[index] * i, current)
             }
             
             current.append(candidates[index])
