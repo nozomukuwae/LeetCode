98569d1353d80417453ec04ec0c05d192fc4dce5
Nozomu Kuwae <nozomukuwae@gmail.com>
2021-05-05 17:14:34 +0900

    [39. Combination Sum] Slight enhancement: Optimize variable usage

--- 39. Combination Sum/Combination Sum.playground/Contents.swift
+++ 39. Combination Sum/Combination Sum.playground/Contents.swift
@@ -1,5 +1,5 @@
-//Runtime: 36 ms, faster than 54.68% of Swift online submissions for Combination Sum.
-//Memory Usage: 14.1 MB, less than 64.03% of Swift online submissions for Combination Sum.
+//Runtime: 32 ms, faster than 64.39% of Swift online submissions for Combination Sum.
+//Memory Usage: 13.9 MB, less than 92.81% of Swift online submissions for Combination Sum.
 
 // Time complexity: O(M^N) where N = count of candidates, M = max quotient of target / candidates
 // Space complexity: O(M^N)
@@ -18,19 +18,18 @@ class Solution {
     func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
         var combinations = [[Int]]()
         
-        let maxCount = target / candidates[0]
         var current = [Int]()
-        var i = 0
+        var p = 0
         
-        while i <= maxCount {
-            if candidates[0] * i == target {
+        while p <= target {
+            if p == target {
                 combinations.append(current)
             } else {
-                findCombination(candidates, 1, &combinations, target - candidates[0] * i, current)
+                findCombination(candidates, 1, &combinations, target - p, current)
             }
             
             current.append(candidates[0])
-            i += 1
+            p += candidates[0]
         }
 
         return combinations
@@ -41,19 +40,18 @@ class Solution {
         guard currentTarget > 0 else { return }
         guard index < candidates.count else { return }
         
-        let maxCount = currentTarget / candidates[index]
         var current = currentCombination
-        var i = 0
+        var p = 0
                 
-        while i <= maxCount {
-            if candidates[index] * i == currentTarget {
+        while p <= currentTarget {
+            if p == currentTarget {
                 combinations.append(current)
             } else {
-                findCombination(candidates, index + 1, &combinations, currentTarget - candidates[index] * i, current)
+                findCombination(candidates, index + 1, &combinations, currentTarget - p, current)
             }
             
             current.append(candidates[index])
-            i += 1
+            p += candidates[index]
         }
     }
 }
